95140684118de3572b9812bf248ea1fd4d83faac
shigeru.hyodo <41170561gel1123@users.noreply.github.com>
2022-07-12 16:08:54 +0900

    chore: サンプルコード追加

---
+++ spring_prj/src/main/java/com/smd/api/service/AccountService.java
@@ -0,0 +1,21 @@
+package com.smd.api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.smd.api.entity.AccountEntity;
+import com.smd.api.repository.AccountRepository;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class AccountService {
+    @Autowired
+    AccountRepository accountRepository;
+
+    public List<AccountEntity> getAccount() {
+        return accountRepository.findAll();
+    }
+}
